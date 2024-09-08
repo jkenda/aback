d91a0646641db4a89cd983f2b22c0169cb660bab
Jakob Kenda <kenda.jakob@gmail.com>
2024-09-08 21:44:14 +0200

    added gdb debug config

--- launch.lua
+++ launch.lua
@@ -2,10 +2,19 @@ local dap = require("dap")
 
 dap.configurations.ocaml = {
     {
+        name = "aback earlybird",
         type = "ocaml",
         request = "launch",
-        name = "aback",
         program = "${workspaceFolder}/_build/default/bin/main.bc",
+        args = { "com", "-i", "core/io.ab" },
+        stopOnEntry = true,
+        cwd = "${workspaceFolder}",
+    },
+    {
+        name = "aback gdb",
+        type = "gdb",
+        request = "launch",
+        program = "${workspaceFolder}/_build/default/bin/main.exe",
         args = { "com", "-i", "core/io.ab" },
         stopOnEntry = true,
         cwd = "${workspaceFolder}",
